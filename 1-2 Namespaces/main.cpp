57a99a44f70958400613121e55dff9cb083ecbd6
杨东 <yangdong0729@outlook.com>
2022-06-05 16:20:16 +0800

    1-2

---
+++ 1-2 Namespaces/main.cpp
@@ -0,0 +1,14 @@
+﻿import <iostream>;
+
+namespace mycode {
+    void foo() {
+        std::cout << "foo() called in the mycode namespace" << std::endl;
+    }
+} // namespace mycode
+
+using namespace mycode;
+
+int main() {
+    mycode::foo(); // Calls the "foo" function in the "mycode" namespace
+    foo();         // implies mycode::foo();
+}
